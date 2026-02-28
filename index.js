3be0fc20bc383619ac8e3f598839e68b4282f15d
Matheus Cardoso <tecocardoso1@gmail.com>
2026-02-28 08:42:32 -0300

    2026_maua_ecm516

---
+++ index.js
@@ -0,0 +1,65 @@
+// // Declarando constantes
+// const nome = "pedro"
+// console.log(nome)
+
+// const idade = 17
+// console.log(idade)
+
+// console.log(typeof(idade))
+
+// // Declarando variaveis
+// let a = 2
+// console.log(a)
+// let passouDeAno = true
+// console.log(passouDeAno)
+// a = 3
+// console.log(a)
+// let b
+// console.log(b)
+// b = 1.5
+// console.log(b)
+
+// // var tbm existe (NAO USAR!)
+// var c = 2
+// c = 3
+// console.log(c)
+
+// var linguagem = "javascript"
+// console.log('Aprendendo ' + linguagem)
+
+// linguagem = "java" //pode colocar o var dnv ou nao
+// console.log(`Aprendendo ${linguagem} agora!`) //interpolacao
+
+// var idade = 18
+// console.log(`Oi, ${nome}`) 
+// if (idade >= 18){
+//     var nome = 'Joao'
+//     console.log(`tchau, ${nome}`)
+// }
+
+// let a = 2
+// console.log(a)
+// console.log(typeof(a))
+// a = "dois"
+// console.log(a)
+// console.log(typeof(a))
+
+
+// coercao implicita e explicita
+// const n1 = 2
+// const n2 = '3'
+
+// const n3 = n1 + n2
+// console.log(n3) //coercao implicita
+
+// const n4 = n1 + Number(n2)
+// console.log(n4) //coercao explicita
+
+//operadores de comparacao por igualdade: == e ===
+// console.log(2 == 2) //true, compara o valor
+// console.log(2 === 2) //true, compara o valor e o tipo
+// console.log(2 == '2') //true, compara o valor
+// console.log(2 === '2') //false, compara o valor e o tipo
+
+//console.log("hello, world!")
+
